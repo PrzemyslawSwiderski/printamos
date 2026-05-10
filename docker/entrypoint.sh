6d74c370fa48909224256fbe4975ccb88e081ba6
Przemyslaw Swiderski <przemyslaw.swiderski7@gmail.com>
2026-05-10 14:02:21 +0200

    Avahi DNS support added 3.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,7 +10,6 @@ sleep 2
 # Overwrite Docker's generated file with our own
 cat <<EOF > /etc/resolv.conf
 nameserver 127.0.0.1
-options ndots:0
 EOF
 
 # Start unbound
@@ -19,15 +18,17 @@ unbound 2>&1 &
 
 sleep 2
 
-# Start avahi2dns
-echo "Starting avahi2dns..."
-avahi2dns -v -p 5354 > /var/log/avahi2dns 2>&1 &
-
-sleep 2
-
 # Start avahi-daemon
 echo "Starting avahi-daemon..."
 avahi-daemon --no-drop-root --no-rlimits --debug > /var/log/cups/avahi 2>&1 &
+
+sleep 5
+
+# Start avahi2dns
+echo "Starting avahi2dns..."
+avahi2dns -v -p 5354 -t 2.5s > /var/log/avahi2dns 2>&1 &
+
+sleep 2
 
 # Start cupsd if available
 if command -v cupsd >/dev/null 2>&1; then
